698b47a5a79b05ad73d6ca105579b5600c5ea674
Cortney Lane <cdlane124@gmail.com>
2015-11-29 09:35:16 -0800

    incomplete, to be modified

--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ By Cortney Lane
 This project uses HTML and JavaScript to generate a table that displays simulated hourly demand at five Top Pot locations. The number of doughnuts sold per hour is then added up into a daily total at the end of the location's table row.
 
 In this project, I learned how to create and append table rows and table data using JavaScript by manipulating the DOM. I also learned how to create a constructor function, and use prototype functions to more efficiently save object data and generate location demand data. The number of customers per hour is a randomized number, falling somewhere between the location-specific minimum and maximum customers per hour, and totals in the table are based off of those generated numbers.
+
+
+How to grab the value of the option selected within the drop-down select menu:
+http://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript
+
+Where I was reminded that the filter() method can help me get an existing object with a specific property value:
+http://stackoverflow.com/questions/13964155/get-javascript-object-from-array-of-objects-by-value-or-property
+
--- script.js
+++ script.js
@@ -7,10 +7,13 @@ var TopPot = function(location, minCustomers, maxCustomers, avgDonuts) {
   this.dailyTotal = 0;
   this.hourlyArray = [];
 
+  // this.getOption = [];
+  // this.getExistingObject = ;
+
 }
 
 // Formula for generating donut numbers based on max and min
-// customers, and average donuts sold per customer. +++++++++++++++++
+// customers, and average donuts sold per customer. +++++++++++++++++++++++++++
 TopPot.prototype.hourlyDonutsBought = function() {
   var hourlyCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
 
@@ -18,7 +21,7 @@ TopPot.prototype.hourlyDonutsBought = function() {
 }
 
 // Now, creating total numbers of donuts sold within each of 11 hrs
-// that the store is open.
+// that the store is open. ++++++++++++++++++++++++++++++++++++++++++++++++++++
 TopPot.prototype.dailyDonutsBought = function() {
   var totalEachHour = 0;
 
@@ -29,17 +32,18 @@ TopPot.prototype.dailyDonutsBought = function() {
 
     };
 
- // Now, we are summing all those 11 hourly donut totals to make one daily total.
+ // Now, we are summing all those 11 hourly donut
+ // totals to make one daily total.
   var hourlyArraySummedUp = this.hourlyArray.reduce(function(a, b) {
     return a + b;
   });
   this.dailyTotal = hourlyArraySummedUp;
 }
 
-// DOM manipulation here. +++++++++++++++++++++++++++++++++++++++
+// DOM manipulation here. +++++++++++++++++++++++++++++++++++++++++++++++++++++
 TopPot.prototype.render = function() {
 
-// Appending donut shop location.
+// Appending donut shop location to table.
     var tr = document.createElement('tr');
     var td = document.createElement('td');
     td.innerHTML = this.location;
@@ -57,11 +61,17 @@ TopPot.prototype.render = function() {
   var appendTotal = document.getElementById('td');
   td.innerHTML = this.dailyTotal;
   tr.appendChild(td);
-  document.getElementById('donut-shop').appendChild(tr);
+  document.getElementById('donutShop').appendChild(tr);
+
+// Append location name to drop-down select box
+  var getDropDown = document.getElementById('existingLocation');
+  var addLocation = document.createElement('option');
+  addLocation.innerHTML = this.location;
+  getDropDown.add(addLocation);
 }
 
 
-// End of functions, begin objects +++++++++++++++++++++++++++++++
+// End of functions, begin objects ++++++++++++++++++++++++++++++++++++++++++++
 
 var loc1 = new TopPot('Downtown', 8, 43, 4.50);
 var loc2 = new TopPot('Capitol Hill', 4, 37, 2.00);
@@ -69,32 +79,91 @@ var loc3 = new TopPot('South Lake Union', 9, 23, 6.33);
 var loc4 = new TopPot('Wedgewood', 2, 28, 1.25);
 var loc5 = new TopPot('Ballard', 8, 58, 3.75);
 
-loc1.dailyDonutsBought();
-loc1.render();
+var allLocations = [loc1, loc2, loc3, loc4, loc5];
 
-loc2.dailyDonutsBought();
-loc2.render();
+allLocations.forEach(function(location) {
+  location.dailyDonutsBought();
+  location.render();
 
-loc3.dailyDonutsBought();
-loc3.render();
+})
 
-loc4.dailyDonutsBought();
-loc4.render();
+// Easter egg event listeners! ************************************************
 
-loc5.dailyDonutsBought();
-loc5.render();
+// Click on the heading, get gif animations to right ******
+var partytime = document.getElementById('header');
 
-// User-input location data
+partytime.addEventListener('click', function() {
+  var span = document.getElementById('partytime');
+  span.innerHTML = "<img src='images/cannon.gif' /><img src='images/noisemaker.gif' />"
+}, false);
+
+// Click on the page break below table, get 15 instances ******
+// of confetti gif, and one instance of cannon gif.
+var tableParty = document.getElementById('party');
+
+tableParty.addEventListener('click', function() {
+  for (var i = 0; i < 15; i++) {
+    party.innerHTML = "<img height='50' src='images/confetti.gif' />";
+  };
+  party.innerHTML +="<img height='50' src='images/cannon.gif' />";
+
+}, false);
+
+// END EASTER EGGS
+
+// User input event listener ++++++++++++++++++++++++++++++++++++++++++++++++++
 
 var userInput = document.getElementById('userInput');
-  console.log('Line 90!');
-userInput.addEventListener('submit', function(e) {
-  e.preventDefault();
 
-  var newDonutShop = new TopPot(event.target.location.value, event.target.minCustomersPh.value, event.target.maxCustomersPh.value, event.target.averageDonutsPc.value);
-  console.log(newDonutShop);
-});
+userInput.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var getSelect = document.getElementById("existingLocation");
+
+  // First, error-checking ****************************************************
+  if (getSelect.value == "selectLocation" && event.target.location.value == '') {
+      alert("D'OH! You didn't enter a location. Try again!");
+    }
+
+  else if (getSelect.value !== "selectLocation" && event.target.location.value !== '') {
+      alert("Can't create a new shop AND update an existing one! Try again.");
+    }
+
+  else {
+
+    if (getSelect.value !== "selectLocation") { // User updates existing location. *
+
+        var getSelect = document.getElementById("existingLocation");
+        var getOption = getSelect.options[getSelect.selectedIndex].text;
+
+        var getExistingObject;
+        for(var i = 0; i < allLocations.length; i++) {
+           var checkObj = allLocations[i];
+           if(checkObj.location == getOption) {
+             getExistingObject = checkObj;
+             delete checkObj;
+             // break;
+           }
+        }
+
+        function deleteRow() {
+          var row = document.getElementById(getExistingObject.location);
+          row.parentNode.removeChild(row);
+        }
+
+        var existingDonutShop = new TopPot(getOption, event.target.minCustomersPh.value, event.target.maxCustomersPh.value, event.target.averageDonutsPc.value);
 
 
-newDonutShop.dailyDonutsBought;
-newDonutShop.render();
+        existingDonutShop.dailyDonutsBought();
+        existingDonutShop.render();
+
+      } else { // User inputs new location and data ***************************
+        var newDonutShop = new TopPot(event.target.location.value, event.target.minCustomersPh.value, event.target.maxCustomersPh.value, event.target.averageDonutsPc.value);
+
+        newDonutShop.dailyDonutsBought();
+        newDonutShop.render();
+      }
+  }
+}, false);
+
+
